5bd8e0a878e0b582c738cc2b159a9e9e8ac04a26
Daniel Barros <97184879+danielbico22@users.noreply.github.com>
2022-02-10 21:04:19 -0300

    Add files via upload

---
+++ bootcamp3/aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bootcamp3
+{
+    public struct Aluno
+    {
+        public string Nome { get; set; }
+
+        public decimal Nota { get; set; } 
+    }
+}
